4e7e549020ca1904a1326b32ec681ef1ba8a57f9
Eli <eli@elibingham.com>
2024-07-17 23:59:00 -0400

    tweak test_ops_interpreter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-mypy --ignore-missing-imports effectful/
+mypy --ignore-missing-imports effectful/ tests/
 isort --check --profile black --diff effectful/ tests/
 black --check effectful/ tests/
 flake8 effectful/ tests/
