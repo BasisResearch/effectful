01e6f332a76a7dc308475701514ba4e08235b655
Raffi Sanna <rvs314@protonmail.com>
2024-07-18 17:57:09 -0400

    Initial integration of `State` into `prompt.py`

--- effectful/handlers/state.py
+++ effectful/handlers/state.py
@@ -11,7 +11,7 @@ A generic mutable state effect with an optional default value.
 ...     return 1 if value == 0 else value * factorial(value - 1)
 >>> factorial(3)
 6
->>> with debug.bound_to(True):
+>>> with interpreter(debug.bound_to(True)):
 ...     print(factorial(3))
 LOG: 3
 LOG: 2
@@ -20,15 +20,15 @@ LOG: 0
 6
 """
 
-from collections import namedtuple
 from dataclasses import dataclass
-from typing import Generic, Protocol, TypeVar
+from typing import Any, Callable, Generic, ParamSpec, TypeVar
 
-from effectful.ops.core import Operation, define, register
+from effectful.ops.core import Interpretation, Operation, define
 from effectful.ops.interpreter import interpreter
 
-
 T = TypeVar("T")
+P = ParamSpec("P")
+Q = ParamSpec("Q")
 
 
 @dataclass
@@ -46,15 +46,32 @@ class Box(Generic[T]):
 class State(Generic[T]):
     get: Operation[[], T]
     set: Operation[[T], None]
+    bound_to: Operation[[T], Interpretation[T | None, T | None]]
 
-    def __init__(self, initial_state: T):
-        box = Box(initial_state)
+    class Nothing:
+        pass
 
-        self.get = Operation(box.get)
-        self.set = Operation(box.set)
+    def __init__(self, initial_state: T | Nothing = Nothing()):
+        if not isinstance(initial_state, self.Nothing):
+            box = Box(initial_state)
+
+            self.get = Operation(box.get)
+            self.set = Operation(box.set)
+        else:
+
+            def raise_a(
+                ex: Callable[..., Exception], *args1, **kwargs1
+            ) -> Callable[..., Any]:
+                def do_raise(*args2, **kwargs2):
+                    raise ex(*(args1 + args2), **(kwargs1 | kwargs2))
+
+                return do_raise
+
+            self.get = Operation(raise_a(ValueError, "Cannot read from an empty box"))
+            self.set = Operation(raise_a(ValueError, "Cannot write to an empty box"))
 
         def bound_to(new: T):
             new_box = Box(new)
-            return interpreter({self.get: new_box.get, self.set: new_box.set})
+            return {self.get: new_box.get, self.set: new_box.set}
 
         self.bound_to = Operation(bound_to)
--- effectful/internals/prompts.py
+++ effectful/internals/prompts.py
@@ -1,9 +1,10 @@
 import contextlib
 import functools
-from typing import Callable, Mapping, Optional, Tuple, TypeVar
+from typing import Any, Callable, Mapping, Optional, Tuple, TypeVar
 
 from typing_extensions import Concatenate, ParamSpec
 
+from effectful.handlers.state import State
 from effectful.ops.core import Interpretation, Operation
 from effectful.ops.interpreter import interpreter
 
@@ -16,7 +17,7 @@ V = TypeVar("V")
 
 @contextlib.contextmanager
 def shallow_interpreter(intp: Interpretation):
-    from ..internals.runtime import get_interpretation
+    from effectful.internals.runtime import get_interpretation
 
     # destructive update: calling any op in intp should remove intp from active
     active_intp = get_interpretation()
@@ -42,9 +43,7 @@ def value_or_result(fn: Callable[P, T]) -> Callable[Concatenate[Optional[T], P],
     return _wrapper
 
 
-@Operation
-def _get_result() -> Optional[T]:
-    return None
+result = State[Any](None)
 
 
 def _set_result(
@@ -52,7 +51,7 @@ def _set_result(
 ) -> Callable[Concatenate[Optional[T], P], T]:
     @functools.wraps(fn)
     def _wrapper(res: Optional[T], *args: P.args, **kwargs: P.kwargs) -> T:
-        return shallow_interpreter({_get_result: lambda: res})(fn)(res, *args, **kwargs)
+        return shallow_interpreter(result.bound_to(res))(fn)(res, *args, **kwargs)
 
     return _wrapper
 
@@ -60,9 +59,7 @@ def _set_result(
 def bind_result(fn: Callable[Concatenate[Optional[T], P], T]) -> Callable[P, T]:
     @functools.wraps(fn)
     def _wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-        return interpreter({_get_result: _get_result.default})(fn)(
-            _get_result(), *args, **kwargs
-        )
+        return interpreter(result.bound_to(None))(fn)(result.get(), *args, **kwargs)
 
     return _wrapper
 
@@ -73,29 +70,27 @@ Prompt = Operation[[Optional[S]], S]
 def bind_prompts(
     unbound_conts: Mapping[Prompt[S], Callable[P, T]],
 ) -> Callable[[Callable[P, T]], Callable[P, T]]:
-    LocalState = Tuple[Tuple, Mapping]
+    Args = Tuple[Tuple, Mapping]
 
-    @Operation
-    def _get_local_state() -> LocalState:
-        raise ValueError("No args stored")
+    args = State[Args]()
 
-    def _set_local_state(fn: Callable[Q, V]) -> Callable[Q, V]:
+    def set_args(fn: Callable[Q, V]) -> Callable[Q, V]:
         @functools.wraps(fn)
         def _wrapper(*a: Q.args, **ks: Q.kwargs) -> V:
-            return interpreter({_get_local_state: lambda: (a, ks)})(fn)(*a, **ks)
+            return interpreter(args.bound_to((a, ks)))(fn)(*a, **ks)
 
         return _wrapper
 
-    def _bind_local_state(fn: Callable[Q, V]) -> Callable[Q, V]:
+    def bind_args(fn: Callable[Q, V]) -> Callable[Q, V]:
         bound_conts = {
             p: _set_result(
                 functools.partial(
-                    lambda k, _: k(*_get_local_state()[0], **_get_local_state()[1]),
+                    lambda k, _: k(*args.get()[0], **args.get()[1]),
                     unbound_conts[p],
                 )
             )
             for p in unbound_conts.keys()
         }
-        return shallow_interpreter(bound_conts)(_set_local_state(fn))
+        return shallow_interpreter(bound_conts)(set_args(fn))
 
-    return _bind_local_state
+    return bind_args
