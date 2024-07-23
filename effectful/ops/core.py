0f40a0d6fbee90853da94b18648a629e8a8241f3
Raffi Sanna <rvs314@protonmail.com>
2024-07-23 10:18:59 -0400

    Flake8 compliance

--- effectful/ops/core.py
+++ effectful/ops/core.py
@@ -72,7 +72,7 @@ def evaluate(term: Term[T]) -> T:
         *(evaluate(a) if isinstance(a, Term) else a for a in term.args),
         **{
             k: (evaluate(v) if isinstance(v, Term) else v)
-            for k, v in term.kwargs.items()
+            for k, v in term.kwargs.items()sfS
         },
     )
 
