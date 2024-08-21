7442ed029c3d8ea8d4587901478958af8f70f348
eb8680 <eb8680@users.noreply.github.com>
2024-08-21 09:31:46 -0400

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-MKL_NUM_THREADS=1 pytest tests/ -s -n auto --cov=effectful/ --cov-report=term-missing ${@-} --cov-report html
+pytest tests/ -s -n auto --cov=effectful/ --cov-report=term-missing ${@-} --cov-report html
