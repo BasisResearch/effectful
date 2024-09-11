a59f2caa0c424d8ed694502553d7d462d5369de2
Eli <eli@elibingham.com>
2024-09-11 14:17:37 -0400

    refactor apply-eval recursion

--- effectful/ops/core.py
+++ effectful/ops/core.py
@@ -4,7 +4,7 @@ from typing import Callable, Generic, Mapping, Sequence, Tuple, Type, TypeVar, U
 
 from typing_extensions import ParamSpec
 
-from effectful.internals.runtime import weak_memoize
+from effectful.internals.runtime import get_interpretation, weak_memoize
 
 P = ParamSpec("P")
 Q = ParamSpec("Q")
@@ -30,7 +30,7 @@ class Operation(Generic[Q, V]):
     default: Callable[Q, V]
 
     def __call__(self, *args: Q.args, **kwargs: Q.kwargs) -> V:
-        return apply(self, *args, **kwargs)  # type: ignore
+        return apply.default(get_interpretation(), self, *args, **kwargs)  # type: ignore
 
 
 @dataclasses.dataclass(frozen=True, eq=True, repr=True, unsafe_hash=True)
@@ -49,16 +49,22 @@ def gensym(t: Type[T]) -> Operation[[], T]:
     return op
 
 
-def apply(op: Operation[P, T], *args: P.args, **kwargs: P.kwargs) -> T:
-    from effectful.internals.runtime import get_interpretation
+@Operation  # type: ignore
+def apply(
+    intp: Interpretation[S, T], op: Operation[P, S], *args: P.args, **kwargs: P.kwargs
+) -> S | T:
+    if op in intp:
+        return intp[op](*args, **kwargs)
+    elif apply in intp:
+        return intp[apply](intp, op, *args, **kwargs)  # type: ignore
+    else:
+        return op.default(*args, **kwargs)
 
-    return get_interpretation().get(op, op.default)(*args, **kwargs)
 
-
-@Operation
-def evaluate(term: Term[T]) -> T:
-    return apply(
-        term.op,
-        *(evaluate(a) if isinstance(a, Term) else a for a in term.args),
-        **{k: (evaluate(v) if isinstance(v, Term) else v) for k, v in term.kwargs},
-    )
+def evaluate(term: T | Term[T]) -> T:
+    if isinstance(term, Term):
+        args = [evaluate(a) for a in term.args]
+        kwargs = {k: evaluate(v) for k, v in term.kwargs}
+        return term.op(*args, **kwargs)
+    else:
+        return term
