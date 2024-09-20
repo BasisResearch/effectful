e7a409ce112a2872481a8429c861da7e22611190
Jack Feser <jack.feser@gmail.com>
2024-09-20 17:04:22 -0400

    fix pytest hook

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,20 @@
-# import pytest
+import pytest
 
-# UNIMPLEMENTED_SUBSTRINGS = [
-#     "infer.JitTrace_ELBO",
-#     "the event_dim arg",
-#     "optim.ClippedAdam",
-#     "infer.TraceMeanField_ELBO",
-# ]
+UNIMPLEMENTED_SUBSTRINGS = [
+    "infer.JitTrace_ELBO",
+    "the event_dim arg",
+    "optim.ClippedAdam",
+    "infer.TraceMeanField_ELBO",
+]
 
 
-# def pytest_runtest_call(item):
-#     try:
-#         item.runtest()
-#     except NotImplementedError as e:
-#         if any(s in str(e) for s in UNIMPLEMENTED_SUBSTRINGS):
-#             pytest.xfail(str(e))
-#         else:
-#             raise e
+@pytest.hookimpl(wrapper=True)
+def pytest_runtest_call(item):
+    try:
+        output = yield
+        return output
+    except NotImplementedError as e:
+        if any(s in str(e) for s in UNIMPLEMENTED_SUBSTRINGS):
+            pytest.xfail(str(e))
+        else:
+            raise e
