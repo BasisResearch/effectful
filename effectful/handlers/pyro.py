6323fc3ccfcd5a56c07b491afce8a275e39dadfe
Eli <eli@elibingham.com>
2024-10-09 15:36:48 -0400

    simplify

--- effectful/handlers/pyro.py
+++ effectful/handlers/pyro.py
@@ -1,83 +1,52 @@
 from typing import Dict, Optional, Any, Mapping
 
-from torch import Tensor, tensor
-from torch.distributions import Distribution
+import torch
+import pyro
 
-import pyro.poutine.util
-from pyro.poutine.messenger import Messenger
-
-from ..ops.core import Operation
+from effectful.ops.core import Operation
 
 
 @Operation
 def pyro_sample(
     name: str,
-    dist: Distribution,
-    obs: Optional[Tensor] = None,
-    mask: Tensor = tensor(True),
-) -> Tensor:
-    raise NotImplementedError()
+    fn: pyro.distributions.torch_distribution.TorchDistributionMixin,
+    *args,
+    obs: Optional[torch.Tensor] = None,
+    obs_mask: Optional[torch.BoolTensor] = None,
+    infer: Optional[pyro.poutine.runtime.InferDict] = None,
+    **kwargs,
+) -> torch.Tensor:
+    """
+    Operation to sample from a Pyro distribution.
+    """
+    return pyro.sample(name, fn, *args, obs=obs, obs_mask=obs_mask, infer=infer, **kwargs)
 
 
-# class EffectfulSampleMessenger(Messenger):
-#     data = {}
+class PyroShim(pyro.poutine.messenger.Messenger):
+    """
+    Handler for Pyro that wraps all sample sites in a custom effectful type.
+    """
+    pyro_type_prefix: str = "__effectful"
 
-#     def _pyro_sample(self, msg: Dict[str, Any]) -> None:
-#         if "name" not in msg:
-#             raise RuntimeError("Sample missing required field 'name'")
-#         if "fn" not in msg:
-#             raise RuntimeError("Sample missing required field 'fn'")
+    _current_site: Optional[str]
 
-#         if "in_pyro_sample_handler" not in msg["infer"]:
-#             msg["value"] = pyro_sample(
-#                 msg["name"], msg["fn"], obs=msg.get("obs"), mask=msg.get("mask")
-#             )
-
-
-class EffectfulSampleMessenger(Messenger):
-    _current_site = None
-
-    def _pyro_sample(self, msg):
-        if (
-            self._current_site == msg["name"]
-            or "in_pyro_sample_handler" in msg["infer"]
-        ):
+    def _pyro_sample(self, msg: pyro.poutine.runtime.Message) -> None:
+        if self._current_site == msg["name"] or \
+                pyro.poutine.util.site_is_subsample(msg) or \
+                pyro.poutine.util.site_is_factor(msg):
             return
 
-        if pyro.poutine.util.site_is_subsample(msg) or pyro.poutine.util.site_is_factor(
-            msg
-        ):
-            return
-
-        if msg["infer"].get("_do_not_observe", None):
-            if (
-                "_markov_scope" in msg["infer"]
-                and getattr(self, "_current_site", None) is not None
-            ):
-                msg["infer"]["_markov_scope"].pop(self._current_site, None)
-            return
-
+        msg["type"] = f"{self.pyro_type_prefix}_{msg['type']}"
         msg["stop"] = True
         msg["done"] = True
-
-        # flags to guarantee commutativity of condition, intervene, trace
-        msg["mask"] = False
-        msg["is_observed"] = False
-        msg["infer"]["is_auxiliary"] = True
-        msg["infer"]["_do_not_trace"] = True
-        msg["infer"]["_do_not_intervene"] = True
-        msg["infer"]["_do_not_observe"] = True
-
-        with pyro.poutine.infer_config(
-            config_fn=lambda msg_: {
-                "_do_not_observe": msg["name"] == msg_["name"]
-                or msg_["infer"].get("_do_not_observe", False)
-            }
-        ):
-            try:
-                self._current_site = msg["name"]
-                msg["value"] = pyro_sample(
-                    msg["name"], msg["fn"], obs=msg.get("obs"), mask=msg.get("mask")
-                )
-            finally:
-                self._current_site = None
+        try:
+            self._current_site = msg["name"]
+            msg["value"] = pyro_sample(
+                msg["name"],
+                msg["fn"],
+                obs=msg["value"] if msg["is_observed"] else None,
+                obs_mask=msg["obs_mask"],
+                infer=msg["infer"],
+            )
+        finally:
+            self._current_site = None
