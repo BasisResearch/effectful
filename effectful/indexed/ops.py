8077230368e876357cbcf5a81492652ec844936f
Jack Feser <jack.feser@gmail.com>
2024-11-14 10:23:56 -0500

    move indices_of implementations to avoid import bug

--- effectful/indexed/ops.py
+++ effectful/indexed/ops.py
@@ -1,4 +1,5 @@
 import functools
+import numbers
 import operator
 from typing import (
     Dict,
@@ -20,6 +21,7 @@ from ..internals.sugar import gensym, partial_eval, sizesof, torch_getitem
 from ..ops.core import Expr, Operation, Term
 from ..ops.function import defun
 
+K = TypeVar("K")
 T = TypeVar("T")
 
 
@@ -208,6 +210,72 @@ def indices_of(value, **kwargs) -> IndexSet:
     :return: A :class:`IndexSet` containing the indices on which the value is supported.
     """
     raise NotImplementedError
+
+
+@indices_of.register
+def _indices_of_number(value: numbers.Number, **kwargs) -> IndexSet:
+    return IndexSet()
+
+
+@indices_of.register
+def _indices_of_bool(value: bool, **kwargs) -> IndexSet:
+    return IndexSet()
+
+
+@indices_of.register
+def _indices_of_none(value: None, **kwargs) -> IndexSet:
+    return IndexSet()
+
+
+@indices_of.register
+def _indices_of_tuple(value: tuple, **kwargs) -> IndexSet:
+    if all(isinstance(v, int) for v in value):
+        return indices_of(torch.Size(value), **kwargs)
+    return union(*(indices_of(v, **kwargs) for v in value))
+
+
+@indices_of.register
+def _indices_of_shape(value: torch.Size, **kwargs) -> IndexSet:
+    name_to_dim: dict[str, int] = (
+        kwargs["name_to_dim"]
+        if "name_to_dim" in kwargs
+        else {name: f.dim for name, f in get_index_plates().items()}
+    )
+    value = value[: len(value) - kwargs.get("event_dim", 0)]
+    return IndexSet(
+        **{
+            name: set(range(value[dim]))
+            for name, dim in name_to_dim.items()
+            if -dim <= len(value) and value[dim] > 1
+        }
+    )
+
+
+@indices_of.register
+def _indices_of_term(value: Term, **kwargs) -> IndexSet:
+    return IndexSet(**{k.__name__: set(range(v)) for (k, v) in sizesof(value).items()})
+
+
+@indices_of.register
+def _indices_of_tensor(value: torch.Tensor, **kwargs) -> IndexSet:
+    if isinstance(value, Term):
+        return _indices_of_term(value, **kwargs)
+    return indices_of(value.shape, **kwargs)
+
+
+@indices_of.register
+def _indices_of_distribution(
+    value: pyro.distributions.Distribution, **kwargs
+) -> IndexSet:
+    kwargs.pop("event_dim", None)
+    return indices_of(value.batch_shape, event_dim=0, **kwargs)
+
+
+@indices_of.register(dict)
+def _indices_of_state(value: Dict[K, T], *, event_dim: int = 0, **kwargs) -> IndexSet:
+    return union(
+        *(indices_of(value[k], event_dim=event_dim, **kwargs) for k in value.keys())
+    )
 
 
 def gather(value, indexset, **kwargs):
