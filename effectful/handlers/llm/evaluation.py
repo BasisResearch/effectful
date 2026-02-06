3d1d814b69fe37a74fbd957d6c5a1018e6ca4b73
Kiran Gopinathan <kiran@basis.ai>
2026-02-06 12:50:10 -0500

    format notebook

--- effectful/handlers/llm/evaluation.py
+++ effectful/handlers/llm/evaluation.py
@@ -536,7 +536,6 @@ def mypy_type_check(
     )
     stub_module = ast.Module(body=full_body, type_ignores=[])
     source = ast.unparse(ast.fix_missing_locations(stub_module))
-    print(f"\n============ SOURCE (BEFORE) ============\n{source}")
     source = (
         subprocess.run(
             [
@@ -555,7 +554,6 @@ def mypy_type_check(
         ).stdout
         or source
     )
-    print(f"\n============ SOURCE (AFTER) ============\n{source}")
 
     stdout, stderr, status = mypy_api.run(
         [
